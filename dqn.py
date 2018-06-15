74e6b99ca98dd263cf7c1469bae9a56ff25b5929
yytdfc <fuchen@foxmail.com>
2018-06-15 17:32:14 +0800

    adapt to pytorch4.0

--- dqn.py
+++ dqn.py
@@ -10,17 +10,14 @@ from torch.autograd import Variable
 from collections import namedtuple
 import env2048
 
-use_cuda = torch.cuda.is_available()
-FloatTensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
-LongTensor = torch.cuda.LongTensor if use_cuda else torch.LongTensor
-ByteTensor = torch.cuda.ByteTensor if use_cuda else torch.ByteTensor
-Tensor = FloatTensor
+# if gpu is to be used
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward', 'ret'))
+                        ('state', 'action', 'next_state', 'reward'))
+
 
 class ReplayMemory(object):
-
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = []
@@ -39,6 +36,7 @@ class ReplayMemory(object):
     def __len__(self):
         return len(self.memory)
 
+
 class DQN(nn.Module):
     def __init__(self):
         super(DQN, self).__init__()
@@ -52,7 +50,7 @@ class DQN(nn.Module):
         self.conv3 = nn.Conv2d(64, 64, kernel_size=2, padding=0)
         self.bn3 = nn.BatchNorm2d(64)
         # 4*4*64 -> 4
-        self.head = nn.Linear(4*4*64, 4)
+        self.head = nn.Linear(4 * 4 * 64, 4)
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
@@ -60,34 +58,33 @@ class DQN(nn.Module):
         x = F.relu(self.bn3(self.conv3(x)))
         return self.head(x.view(x.size(0), -1))
 
-def convert_state2net(state):
-    n = Tensor(16,16).fill_(0)
-    for i,c in enumerate(state.flatten()):
-        n[c,i]=1 #2.0**c
-    n.resize_(1,16,4,4)
-    return n
 
 class DQNPlayer():
-    def __init__(self, name = 'DQN Player', eps = 0.9):
+    def __init__(self, name='DQN Player', eps=0.9):
         self.name_ = name
         self.eps_ = eps
         self.eps_end_ = 0.05
         self.batch_size_ = 128
         self.gamma_ = 0.999
-        self.dqn_ = DQN()
-        if use_cuda:
-            self.dqn_.cuda()
-        self.optimizer_ = optim.RMSprop(self.dqn_.parameters())
-        self.memory_ = ReplayMemory(1000)
+        self.policy_net_ = DQN().to(device)
+        self.target_net_ = DQN().to(device)
+        self.target_net_.load_state_dict(self.policy_net_.state_dict())
+        self.target_net_.eval()
+        self.optimizer_ = optim.RMSprop(self.policy_net_.parameters())
+        self.memory_ = ReplayMemory(10000)
         self.average_ = 0
         self.trained_ = 0
+        self.TARGET_UPDATE = 10
+
     def select_action(self, state, greedy=False):
-        if greedy or np.random.random()>self.eps_:
-            n = convert_state2net(state)
-#            n.resize_(1,16,4,4)
-            return torch.max(self.dqn_(Variable(n)).data,1)[1][0]
+        if greedy or np.random.random() > self.eps_:
+            #            return torch.max(self.policy_net_(Variable(n)).data, 1)[1][0]
+            with torch.no_grad():
+                return self.policy_net_(state).max(1)[1].view(1, 1)
         else:
-            return np.random.randint(4)
+            return torch.tensor(
+                [[random.randrange(4)]], device=device, dtype=torch.long)
+
     def optimize_model(self):
         if len(self.memory_) < self.batch_size_:
             return
@@ -95,126 +92,121 @@ class DQNPlayer():
         # Transpose the batch (see http://stackoverflow.com/a/19343/3343043 for
         # detailed explanation).
         batch = Transition(*zip(*transitions))
-    
+
         # Compute a mask of non-final states and concatenate the batch elements
-        non_final_mask = ByteTensor(tuple(map(lambda s: s is not None,
-                                              batch.next_state)))
-#        final_mask = ByteTensor(tuple(map(lambda s: s is None,
-#                                              batch.next_state)))
-        # We don't want to backprop through the expected action values and volatile
-        # will save us on temporarily changing the model parameters'
-        # requires_grad to False!
-        non_final_next_states = Variable(torch.cat([s for s in batch.next_state
-                                                    if s is not None]),
-                                         volatile=True)
-        state_batch = Variable(torch.cat(batch.state))
-        action_batch = Variable(torch.cat(batch.action))
-        reward_batch = Variable(torch.cat(batch.reward))
-        return_batch = Variable(torch.cat(batch.ret))
+        non_final_mask = torch.tensor(
+            tuple(map(lambda s: s is not None, batch.next_state)),
+            device=device,
+            dtype=torch.uint8)
+        non_final_next_states = torch.cat(
+            [s for s in batch.next_state if s is not None])
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
         # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
         # columns of actions taken
-        state_action_values = self.dqn_(state_batch).gather(1, action_batch)
-    
+        state_action_values = self.policy_net_(state_batch).gather(
+            1, action_batch)
+
         # Compute V(s_{t+1}) for all next states.
-        next_state_values = Variable(torch.zeros(self.batch_size_).type(Tensor))
-        next_state_values[non_final_mask] = self.dqn_(non_final_next_states).max(1)[0]
-#        next_state_values[final_mask] = reward_batch[final_mask]
-        # Now, we don't want to mess up the loss with a volatile flag, so let's
-        # clear it. After this, we'll just end up with a Variable that has
-        # requires_grad=False
-        next_state_values.volatile = False
+        next_state_values = torch.zeros(self.batch_size_, device=device)
+        next_state_values[non_final_mask] = self.target_net_(
+            non_final_next_states).max(1)[0].detach()
         # Compute the expected Q values
-        expected_state_action_values = (next_state_values * self.gamma_) \
-                                        - reward_batch + return_batch
-#        print(batch.state[0],batch.next_state[0])
-#        print(state_action_values.data[0][0],expected_state_action_values.data[0])
-        
+        expected_state_action_values = (
+            next_state_values * self.gamma_) + reward_batch
+
         # Compute Huber loss
-        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values)
-#        print(loss.data[0])
+        loss = F.smooth_l1_loss(state_action_values,
+                                expected_state_action_values.unsqueeze(1))
+
         # Optimize the model
         self.optimizer_.zero_grad()
         loss.backward()
-        for param in self.dqn_.parameters():
+        for param in self.policy_net_.parameters():
             param.grad.data.clamp_(-1, 1)
         self.optimizer_.step()
-#        input()
+
     def training(self, num_episodes):
+        last10 = []
         env = env2048.Env2048()
-        last10=[]
         for i_episode in range(num_episodes):
-            # Initialisze the environment and state
+            # Initialize the environment and state
             env.reset()
-            state = env.get_state()
-            while 1:
+            state = torch.tensor(env.to_tensor())
+            while (1):
                 # Select and perform an action
                 action = self.select_action(state)
-                _, reward, done = env.step(action)
-                
-                # Observe new state
+                _, reward, done, _ = env.step(action.item())
+                reward = torch.tensor(
+                    [reward], device=device, dtype=torch.float32)
+
                 if not done:
-                    next_state = env.get_state()
-                    if reward>=0:
-                        # Store the transition in memory
-                        self.memory_.push(convert_state2net(state),
-                                          LongTensor([[action]]),
-                                          convert_state2net(next_state),
-                                          Tensor(1).fill_(float(reward)),
-                                          Tensor(1).fill_(0))
+                    next_state = torch.tensor(env.to_tensor())
                 else:
                     next_state = None
-                    # Store the transition in memory
-                    self.memory_.push(convert_state2net(state),
-                                      LongTensor([[action]]),
-                                      None,
-                                      Tensor(1).fill_(float(reward)),
-                                      Tensor(1).fill_(float(env.get_return())))
+                # Store the transition in memory
+                self.memory_.push(state, action, next_state, reward)
+
                 # Move to the next state
                 state = next_state
-    
+
                 # Perform one step of the optimization (on the target network)
                 self.optimize_model()
-                
                 if done:
-                    
                     break
-#            self.average_ = (env.get_return() + self.average_ 
+#                if done:
+#                    episode_durations.append(t + 1)
+#                    plot_durations()
+#                    break
+# Update the target network
+            if i_episode % self.TARGET_UPDATE == 0:
+                self.target_net_.load_state_dict(self.policy_net_.state_dict())
+
+
+#            self.average_ = (env.get_return() + self.average_
 #                             * self.trained_) / (self.trained_ + 1)
             last10.append(env.get_return())
-            if len(last10)>10:
+            if len(last10) > 10:
                 last10.pop(0)
             self.average_ = np.average(last10)
             self.trained_ += 1
-            self.eps_ = self.eps_end_ + np.exp(-self.trained_/200) * (0.9-0.05)
-            print('epi %3d, score %5d, avg %5.0f, eps %.4f'%(self.trained_,
-                    env.get_return(), self.average_, self.eps_))
+            self.eps_ = self.eps_end_ + np.exp(
+                -self.trained_ / 200) * (0.9 - 0.05)
+            print('epi %3d, score %5d, avg %5.0f, eps %.4f' %
+                  (self.trained_, env.get_return(), self.average_, self.eps_))
+
+
 def __test__():
     net = DQN()
-    s=np.array([[0, 0, 0, 0],
-                [1, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 1, 0]], dtype=np.int8)
-    n=convert_state2net(s)
-    n.resize_(1,16,4,4)
+    s = np.array(
+        [[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0]],
+        dtype=np.int8)
+    n = convert_state2net(s)
+    n.resize_(1, 16, 4, 4)
     print(net.forward(Variable(n)))
-    
+
+
 if __name__ == '__main__':
     n_episodes = 200
     player = DQNPlayer()
     player.training(n_episodes)
-    env2048.test_player(player, 10)
-    s=np.array([[0, 0, 0, 0],
-                [1, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 1, 0]], dtype=np.int8)
-    print(player.dqn_.forward(Variable(convert_state2net(s))))
-    s=np.array([[0, 0, 0, 0],
-                [3, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 3, 0]], dtype=np.int8)
-    print(player.dqn_.forward(Variable(convert_state2net(s))))
-    s=np.array([[0, 0, 0, 0],
-                [1, 2, 3, 0],
-                [1, 2, 3, 0],
-                [0, 0, 1, 0]], dtype=np.int8)
-    print(player.dqn_.forward(Variable(convert_state2net(s))))
+    #    player.eps_=0.1
+    #    env2048.test_player(player, 10)
+    s = np.array(
+        [[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0]],
+        dtype=np.int8)
+    print(player.policy_net_.forward(torch.tensor(env2048.state2tensor(s))))
+    s = np.array(
+        [[0, 0, 0, 0], [3, 0, 0, 0], [0, 0, 0, 0], [0, 0, 3, 0]],
+        dtype=np.int8)
+    print(player.policy_net_.forward(torch.tensor(env2048.state2tensor(s))))
+    s = np.array(
+        [[0, 0, 0, 0], [1, 2, 3, 0], [1, 2, 3, 0], [0, 0, 1, 0]],
+        dtype=np.int8)
+    print(player.policy_net_.forward(torch.tensor(env2048.state2tensor(s))))
+    s = np.array(
+        [[1, 2, 0, 2], [4, 0, 0, 1], [2, 0, 0, 0], [0, 0, 0, 1]],
+        dtype=np.int8)
+    print(player.policy_net_.forward(Variable(convert_state2net(s))))
--- env2048.py
+++ env2048.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-ACTION_MAP = [ '↑','↓','←','→']
+ACTION_MAP = ['↑', '↓', '←', '→']
+
+
+def state2tensor(state):
+    t = np.zeros((16, 16), dtype=np.float32)
+    for i, c in enumerate(state.flatten()):
+        t[c, i] = 1  #2.0**c
+    t.resize(1, 16, 4, 4)
+    return t
+
+
 class Env2048(object):
     def __init__(self, dim=4, base=2, state=None):
         self.dim_ = dim
@@ -9,129 +19,157 @@ class Env2048(object):
         self.start_tiles_ = 2
         self.score_ = 0
         if state is None:
-            self.state_ = np.zeros((self.dim_,self.dim_), dtype=np.int8)
+            self.state_ = np.zeros((self.dim_, self.dim_), dtype=np.int8)
             self.add_start_tile()
         else:
             self.state_ = state.copy()
+
     def __str__(self):
-        conver2char = lambda num :'%5d'%(self.base_**num) if num>0 else ' '*5
-        demarcation = ( '+' + '-'*5 ) * self.dim_ + '+\n'
+        conver2char = lambda num: '%5d' % (self.base_**num) if num > 0 else ' ' * 5
+        demarcation = ('+' + '-' * 5) * self.dim_ + '+\n'
         ret = demarcation
-        ret += demarcation.join(['|'+'|'.join([conver2char(num) 
-                        for num in row])+'|\n' for row in self.state_])
+        ret += demarcation.join([
+            '|' + '|'.join([conver2char(num) for num in row]) + '|\n'
+            for row in self.state_
+        ])
         ret += demarcation
         return ret
+
     def __repr__(self):
         return self.__str__(self)
+
     def set_state(self, state):
         self.state_ = state.copy()
         self.score_ = 0
+
     def get_state(self):
         return self.state_.copy()
+
+    def to_tensor(self):
+        return state2tensor(self.state_)
+
     def reset(self):
-        self.state_ = np.zeros((self.dim_,self.dim_), dtype=np.int8)
+        self.state_ = np.zeros((self.dim_, self.dim_), dtype=np.int8)
         self.add_start_tile()
         self.score_ = 0
+
     def step(self, action):
-        score = self.score_
+        score0 = self.score_
         if self.move(action):
             self.add_random_tile()
-            return self.state_.copy(), self.score_-score, self.is_terminate()
+            return self.state_.copy(), self.score_ - score0, self.is_terminate(
+            ), ''
         else:
-            return self.state_.copy(), -8, self.is_terminate()
+            return self.state_.copy(), 0, self.is_terminate(), ''
+
     def get_return(self):
         return self.score_
+
     ## functions according to 2048/js/game_manager.js
     def add_start_tile(self):
         for i in range(self.start_tiles_):
             self.add_random_tile()
+
     def add_random_tile(self):
         avilable = self.available_cell()
         if avilable.size:
-            self.state_.itemset(np.random.choice(avilable),
-                    1 if np.random.random() < 0.9 else 2)
+            self.state_.itemset(
+                np.random.choice(avilable), 1
+                if np.random.random() < 0.9 else 2)
             return False
         else:
             return True
+
     def available_cell(self):
-        return np.argwhere(self.state_.flatten()==0).flatten()
+        return np.argwhere(self.state_.flatten() == 0).flatten()
+
     def is_terminate(self):
-        if self.available_cell().size==0:
+        if self.available_cell().size == 0:
             if (self.state_[:,:-1]==self.state_[:,1:]).any() \
                 or (self.state_[:-1,:]==self.state_[1:,:]).any():
                 return False
             else:
                 return True
         return False
-    
+
     def emit_line(self, line):
         changed = False
         # [ i, _ , j , _ ]
-        i,j = 0,1
-        while j<self.dim_:
-            if line[j]==0:
-                j+=1
-            elif line[i]==0:
+        i, j = 0, 1
+        while j < self.dim_:
+            if line[j] == 0:
+                j += 1
+            elif line[i] == 0:
                 changed = True
-                line[i]=line[j]
-                line[j]=0
-                j+=1
-            elif line[i]==line[j]:
+                line[i] = line[j]
+                line[j] = 0
+                j += 1
+            elif line[i] == line[j]:
                 changed = True
-                line[i]+=1
-                line[j]=0
+                line[i] += 1
+                line[j] = 0
                 self.score_ += self.base_**line[i]
-                i+=1
-                j+=1
+                i += 1
+                j += 1
             else:
-                i+=1
-            if j==i:
-                j+=1
+                i += 1
+            if j == i:
+                j += 1
         return line, changed
+
     def move(self, direct):
         changed = False
-        if direct==0: # up
+        if direct == 0:  # up
             for i in range(4):
-                self.state_[:,i], c = self.emit_line(self.state_[:,i])
+                self.state_[:, i], c = self.emit_line(self.state_[:, i])
                 changed = changed or c
-        elif direct==1: # down
+        elif direct == 1:  # down
             for i in range(4):
-                self.state_[-1::-1,i], c = self.emit_line(self.state_[-1::-1,i])
+                self.state_[-1::-1, i], c = self.emit_line(
+                    self.state_[-1::-1, i])
                 changed = changed or c
-        elif direct==2: # left
+        elif direct == 2:  # left
             for i in range(4):
-                self.state_[i,:], c = self.emit_line(self.state_[i,:])
+                self.state_[i, :], c = self.emit_line(self.state_[i, :])
                 changed = changed or c
-        elif direct==3: # right
+        elif direct == 3:  # right
             for i in range(4):
-                self.state_[i,-1::-1], c = self.emit_line(self.state_[i,-1::-1])
+                self.state_[i, -1::-1], c = self.emit_line(
+                    self.state_[i, -1::-1])
                 changed = changed or c
         return changed
-    
+
+
 class RandomPlayer():
     """
     A player which will take random move
     on 100 simulations: average score 1093, max score 2736
     """
-    def __init__(self, name = 'Random Player'):
+
+    def __init__(self, name='Random Player'):
         self.name_ = name
+
     def select_action(self, state):
         return np.random.randint(4)
-    
+
+
 class OneStepPlayer():
     """
     A player which will search one step forward.
     on 100 simulations: average score 1811, max score 6192
     """
-    def __init__(self, name = 'One Step Player'):
+
+    def __init__(self, name='One Step Player'):
         self.name_ = name
+
     def select_action(self, state):
         rewards = [Env2048(state=state).step(i)[1] for i in range(4)]
-        if np.max(rewards)>0:
+        if np.max(rewards) > 0:
             return np.argmax(rewards)
         else:
             return np.random.randint(4)
-        
+
+
 class MultiStepPlayer():
     """
     A player which will search steps depth.
@@ -139,35 +177,40 @@ class MultiStepPlayer():
         2 steps depth: average score 7648, max score 16132
         3 steps depth: average score 8609, max score 16248
     """
-    def __init__(self, name = 'Mutil Step Player', steps=2):
+
+    def __init__(self, name='Mutil Step Player', steps=2):
         self.name_ = name
         self.n_steps_ = steps
+
     def select_action(self, state):
         rewards = np.zeros(4, dtype=np.int32)
         for i in range(4):
             env = Env2048(state=state)
             new_state, rewards[i], _ = env.step(i)
-            for _ in range(self.n_steps_-1):
-                new_state, reward, _ = env.step(OneStepPlayer().select_action(new_state))
+            for _ in range(self.n_steps_ - 1):
+                new_state, reward, _ = env.step(
+                    OneStepPlayer().select_action(new_state))
                 rewards[i] += reward
-        if np.max(rewards)>0:
+        if np.max(rewards) > 0:
             return np.argmax(rewards)
         else:
             return np.random.randint(4)
 
+
 def play_once(env, player):
-    epoch=1
+    epoch = 1
     while 1:
         a = player.select_action(env.get_state())
         _, r, t = env.step(a)
         if t:
             break
-        epoch+=1
+        epoch += 1
         # print('Ehoch %d: step %s'%(epoch, a))
     ret = env.get_return()
-#    print('Ehoch %d: score %d'%(epoch, ret))
-#    print(env)
+    #    print('Ehoch %d: score %d'%(epoch, ret))
+    #    print(env)
     return ret
+
 
 def test_player(player, n_episodes):
     env = Env2048()
@@ -177,23 +220,24 @@ def test_player(player, n_episodes):
         env.reset()
         ret = play_once(env, player)
         sum_ret += ret
-        if ret>max_ret:
-#            print(env)
+        if ret > max_ret:
+            #            print(env)
             max_ret = ret
-    print('%s average score %d, max score %d'%(player.name_, sum_ret/n_episodes, max_ret))
+    print('%s average score %d, max score %d' %
+          (player.name_, sum_ret / n_episodes, max_ret))
+
 
 def __test__():
-    s=np.array([[0, 0, 0, 0],
-                [1, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 1, 0]], dtype=np.int8)
+    s = np.array(
+        [[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0]],
+        dtype=np.int8)
     env = Env2048(state=s)
     print(env)
     env.step(1)
     print(env)
 
+
 if __name__ == '__main__':
     n_episodes = 100
     test_player(RandomPlayer(), n_episodes)
     test_player(MultiStepPlayer(steps=1), n_episodes)
-    
